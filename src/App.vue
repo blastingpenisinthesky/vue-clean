b937eaf7053420e076136e1a6a0e74f894272b51
dimi <hodzicomer@hotmail.com>
2025-09-17 16:52:10 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav class="bg-gray-950 text-white p-4 space-x-4">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped></style>
